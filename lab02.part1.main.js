8b39f25280f834b19171772e0d7a3dad2e48a0c9
shubhsingh10 <shubh.singh@centurylink.com>
2019-12-30 07:29:59 +0000

    Lab 2 part 1 of 2

--- lab02.part1.main.js
+++ lab02.part1.main.js
@@ -42,7 +42,6 @@ function getFirstIpAddress(cidrStr, callback) {
     // Node.js convention is to pass error data as the first argument to a callback.
     // The IAP convention is to pass returned data as the first argument and error
     // data as the second argument to the callback function.
-
     return callback(firstIpAddress, callbackError);
 }
 
